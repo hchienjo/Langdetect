8bb1c369e6556fbfea827575eae12640b2ea2027
Charlton_A <asino4charlton@gmail.com>
2016-06-08 12:38:14 +0300

    Added a simpe API

--- app.py
+++ app.py
@@ -29,5 +29,20 @@ def result():
         return render_template('find.html', **output)
 
 
+@app.route('/api/<string:sentence>', methods=['GET'])
+def apiResult(sentence):
+    '''
+    Returns JSON format form data.Sentence is passed to url with + to act as spaces ie "habari+yako"
+    '''
+    cleanSentence = sentence.split('+')
+    cleanSentence = ' '.join(cleanSentence)
+    get1, get2, get3 = cld2.detect(sentence)
+    reliability = (get1)
+    lang = (get3[0][0])
+    match = ('{0:.4f} %'.format(get3[0][2]))
+    output = {'reliablity': reliability, 'lang': lang, 'match': match}
+    return jsonify(output)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
